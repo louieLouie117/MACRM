9825323a24ebfbbc178123f49571804defc99927
Luis Cardona <cardona-luis@outlook.com>
2021-11-29 05:49:42 -0600

    ServiceJobModel-UpdatedBool

--- Models/ServiceJob.cs
+++ Models/ServiceJob.cs
@@ -14,17 +14,25 @@ namespace KcPilot.Models
         [Key]
         public int ServiceJobId { get; set; }
 
-        public string SJNumber { get; set; }
 
         [Column(TypeName = "nvarchar(24)")]
         [EnumDataType(typeof(JobStatus))]
         public JobStatus JobStatus { get; set; }
         public string JobColor { get; set; }
+        public string SJNumber { get; set; }
 
 
+        public bool InWarrantyCustomer { get; set; }
         public string CustomerName { get; set; }
         public string CustomerPhoneNumber { get; set; }
-
+        public bool GreetedCustomer { get; set; }
+        public bool ExplainDiagnosticFees { get; set; }
+        public bool ConfirmServiceAndShippingAddress { get; set; }
+        public bool InformCallMaybeRecordedForQualityAndTraining { get; set; }
+        public bool ConfirmModelAndSerialNumber { get; set; }
+        public bool AskedCovidQuestions { get; set; }
+        public bool SetAnAppointmentDate { get; set; }
+        public DateTime AppointmentDate { get; set; }
 
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
