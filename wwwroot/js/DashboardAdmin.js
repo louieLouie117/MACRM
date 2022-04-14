02dba9470447e9efcb54802e40a4b1cfec851b4b
Luis Cardona <cardona-luis@outlook.com>
2022-04-14 07:31:13 -0500

    APIGenerator-MaketInfo

--- wwwroot/js/DashboardAdmin.js
+++ wwwroot/js/DashboardAdmin.js
@@ -4,16 +4,16 @@
      document.getElementById("apiServiceJobNumber").value = "SNWQ" + Math.floor(1000000 + Math.random() * 9000) + "-1";
      document.getElementById("apiSamsungJobNumber").value = "416" + Math.floor(10000 + Math.random() * 9000);
 
+   
      var warrantyOptions = ['COD', 'Warranty'];
      var Warranty = warrantyOptions[Math.floor(Math.random()*warrantyOptions.length)];
      document.getElementById("apiInWarranty").value = Warranty;
 
      
-    
-     var WindowOptions = ['AM', 'PM'];
-     var Window = WindowOptions[Math.floor(Math.random()*WindowOptions.length)];
-     document.getElementById("apiAppointmentWindow").value = Window;
-
+     var stateOptions = ['Kansas', 'Missouri'];
+     var Warranty = stateOptions[Math.floor(Math.random()*stateOptions.length)];
+     document.getElementById("apiState").value = Warranty;
+   
 
      $.getJSON(`https://random-data-api.com/api/address/random_address`, data => {
         console.log(data, "data is here")
@@ -34,6 +34,25 @@
      var Window = ProductOptions[Math.floor(Math.random()*ProductOptions.length)];
      document.getElementById("apiProductLine").value = Window;
 
+     var WindowOptions = ['AM', 'PM'];
+     var Window = WindowOptions[Math.floor(Math.random()*WindowOptions.length)];
+     document.getElementById("apiAppointmentWindow").value = Window;
+
+
+     var MarketOptions = ['Kansas City', 'Kansas City-OEM'];
+     var Market = MarketOptions[Math.floor(Math.random()*MarketOptions.length)];
+     document.getElementById("apiMarket").value = Market;
+
+    if(Market === "Kansas City"){
+     document.getElementById("apiMarketCode").value = "BF445";
+
+    }
+
+    if(Market === "Kansas City-OEM"){
+        document.getElementById("apiMarketCode").value = "BF798";
+   
+       }
+     
     };
 
 
