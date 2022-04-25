4ddf7d2242c80362cc871a988b050527d314e6d9
Luis Cardona <cardona-luis@outlook.com>
2022-04-25 06:40:01 -0500

    NewJobRechedBackend

--- Controllers/HomeConroller.cs
+++ Controllers/HomeConroller.cs
@@ -265,6 +265,25 @@ namespace KcPilot.Controllers
         }
 
 
+        [HttpPost("NewJobMethod")]
+        public JsonResult NewJobMethod(Job UserInputData)
+
+        {
+
+            int UserIdInSession = (int)HttpContext.Session.GetInt32("UserId");
+            UserInputData.UserId = UserIdInSession;
+            System.Console.WriteLine("You have reached the back end of NewJobMethod");
+
+            _context.Add(UserInputData);
+            _context.SaveChanges();
+            List<Job> JobList = _context.Jobs.ToList();
+
+            return Json(new { Result = JobList });
+
+        }
+
+
+
 
 
     }
